1366a3656e853d52f9bb3b657f7990eba6d918ff
Priyanshiguptaa <guppriyanshi@gmail.com>
2024-07-07 17:32:31 -0700

    formatting code

--- compute_engine.py
+++ compute_engine.py
@@ -4,19 +4,20 @@ import anthropic
 import json
 # Load environment variables from .env file
 
-def text_to_json(text):
+def remove_first_and_last_line_from_text(text):
+    lines = text.splitlines()
+    if len(lines) > 2:
+        lines = lines[1:-1]  # Remove the first and last line
+    return '\n'.join(lines)
+def parse_message_to_json(message):
+    
+    text = remove_first_and_last_line_from_text(message) 
     try:
         json_data = json.loads(text)
         return json_data
     except json.JSONDecodeError as e:
         print(f"Error decoding JSON: {e}")
         return None
-
-def remove_first_and_last_line_from_text(text):
-    lines = text.splitlines()
-    if len(lines) > 2:
-        lines = lines[1:-1]  # Remove the first and last line
-    return '\n'.join(lines)
 
 def read_json_configuration(f_config):
     with open(f_config) as json_data:
@@ -52,6 +53,5 @@ def generate_json(obj):
             }
         ]
     ).content[0].text
-    modified_text = remove_first_and_last_line_from_text(message)
-    json_data = text_to_json(modified_text)
+    json_data = parse_message_to_json(message)
     return json_data
